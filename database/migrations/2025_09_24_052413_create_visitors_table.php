65d46907e58c3f9d8526f32635c1051c4225e844
Fikri <fan10062003@gmail.com>
2026-05-24 11:25:32 +0700

    refactor(visitor): improve visitor table structure

--- database/migrations/2025_09_24_052413_create_visitors_table.php
+++ database/migrations/2025_09_24_052413_create_visitors_table.php
@@ -14,6 +14,14 @@ return new class extends Migration
         Schema::create('visitors', function (Blueprint $table) {
             $table->id();
             $table->morphs('visitable');
+            $table->foreignId('user_id')
+                ->nullable()
+                ->constrained()
+                ->nullOnDelete();
+            $table->string('session_id')->nullable();
+            $table->string('ip_address', 45)->nullable();
+            $table->string('user_agent')->nullable();
+            $table->string('referer')->nullable();
             $table->json('data')->nullable();
             $table->timestamps();
         });
